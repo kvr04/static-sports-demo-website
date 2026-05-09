f589e78f10ee61a9ad21f03a0b9c0a890982ae8c
kvr04 <korikana.venkataramana04@gmail.com>
2026-05-09 12:30:00 +0530

    updated the js server port

--- server.js
+++ server.js
@@ -62,7 +62,11 @@ Timing: ${timing}
     }
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-    console.log("Server Running on Port 3000");
+app.listen(PORT, () => {
+
+    console.log(
+        `Server Running on Port ${PORT}`
+    );
 });
